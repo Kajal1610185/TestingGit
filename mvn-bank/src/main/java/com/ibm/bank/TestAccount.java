2df033a654d952bb94460693cb92ec4b3e264fa9
kajal1610185@akgec.ac.in <ibmjavafsdmr09@iiht.tech>
2021-04-05 10:04:40 +0530

    work done

---
+++ mvn-bank/src/main/java/com/ibm/bank/TestAccount.java
@@ -0,0 +1,30 @@
+package com.ibm.bank;
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		Account a1=new Account("Polo", 1000);
+		a1.deposit(2000);
+		a1.summary();
+		try {
+			a1.withdraw(5000);
+		} catch (BalanceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Savings s1= new Savings("mona");
+		try {
+			s1.withdraw(5);
+		} catch (BalanceException e) {
+			// TODO Auto-generated catch block
+		//e.printStackTrace(); //for debugging/troubleshooting user: developer
+		/*	System.out.println(e);*/ //for system/application audit: user: Loggers
+			System.out.println(e.getMessage());//for end user
+		} //insufficient balance
+	}
+
+}
+
+
+
